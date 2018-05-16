bb7d894997452d2b1e68e56d3f5b92b2dc711593
zraina <drefiom@gmail.com>
2018-05-16 09:32:37 +0100

    added more columns

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
-from utils import outcome
+from utils import result, home_score, away_score
+from utils import goal_goal, over_1, over_2, over_3, over_4
 import datetime
 
 count = 0
@@ -8,12 +9,12 @@ soup = BeautifulSoup(open('vfl.html'), 'html.parser')
 weeks = soup.findAll('td', {'style':'width: 33%; text-align:center; background-color: #E1E3E6; padding: 7px'})
 
 f = open("data.csv", "w")
-f.write("home, score, away, outcome \n")
+f.write("home, score, away, result, home_score, away_score, goal_goal, over_1, over_2, over_3, over_4 \n")
 
 for week in weeks:
     weekn = week.findAll('tr')
     count = count +1
-    print("Week " + str(count) + " Ported")
+    print("Week " + str(count) + " ...")
 
     for match in weekn:
         match1 = match.findAll('td')
@@ -21,7 +22,7 @@ for week in weeks:
             home = match1[0].text
             score = match1[1].text
             away = match1[2].text
-            f.write(home + "," + score + "," + away + "," + outcome(score) + "\n")
+            f.write(home + "," + score + "," + away + "," + result(score) + "," + home_score(score) + "," + away_score(score) + "," + goal_goal(score) + "," + over_1(score) + "," + over_2(score) + "," + over_3(score) + "," + over_4(score) + "\n")
 
 f.close()
-print("Data Ported to CSV format")
+print("Porting Successfully Finished")
