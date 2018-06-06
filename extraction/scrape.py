f5ce473116a9deeb49107bba29e25d107e1f4c1b
zraina <drefiom@gmail.com>
2018-06-06 16:42:24 +0100

    extraction completed

--- extraction/scrape.py
+++ extraction/scrape.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
-from utils import result, home_score, away_score
+from utils import result, home_score, away_score, hodd, dodd, aodd
 from utils import goal_goal, over_1, over_2, over_3, over_4
 import datetime
 import os
@@ -7,7 +7,7 @@ import os
 directory = os.fsencode("database")
 
 f = open("data.csv", "w")
-f.write("home,score,away,result,home_score,away_score,goal_goal,over_1,over_2,over_3,over_4\n")
+f.write("home,score,away,result,home_score,away_score,goal_goal,over_1,over_2,over_3,over_4,home_odd,draw_odd,away_odd\n")
 
 
 for file in os.listdir(directory):
@@ -31,7 +31,100 @@ for file in os.listdir(directory):
                 home = match1[0].text
                 score = match1[1].text
                 away = match1[2].text
-                f.write(home + "," + score + "," + away + "," + result(score) + "," + home_score(score) + "," + away_score(score) + "," + goal_goal(score) + "," + over_1(score) + "," + over_2(score) + "," + over_3(score) + "," + over_4(score) + "\n")
+
+                if home == 'LEI':
+                    s_home = 'lei'
+                if home == 'WBA':
+                    s_home = 'wba'
+                if home == 'WHU':
+                    s_home = 'whu'
+                if home == 'MNU':
+                    s_home = 'mnu'
+                if home == 'STC':
+                    s_home = 'stc'
+
+                if home == 'CHE':
+                    s_home = 'che'
+                if home == 'WAT':
+                    s_home = 'wat'
+                if home == 'BRI':
+                    s_home = 'bri'
+                if home == 'NWC':
+                    s_home = 'nwc'
+                if home == 'BOU':
+                    s_home = 'bou'
+
+                if home == 'SWA':
+                    s_home = 'swa'
+                if home == 'BUR':
+                    s_home = 'bur'
+                if home == 'ARS':
+                    s_home = 'ars'
+                if home == 'TOT':
+                    s_home = 'tot'
+                if home == 'EVE':
+                    s_home = 'eve'
+
+                if home == 'HUD':
+                    s_home = 'hud'
+                if home == 'LIV':
+                    s_home = 'liv'
+                if home == 'CRY':
+                    s_home = 'cry'
+                if home == 'SOU':
+                    s_home = 'sou'
+                if home == 'MNC':
+                    s_home = 'mnc'
+
+################################################################
+################################################################
+
+                if away == 'LEI':
+                    s_away = 'lei'
+                if away == 'WBA':
+                    s_away = 'wba'
+                if away == 'WHU':
+                    s_away = 'whu'
+                if away == 'MNU':
+                    s_away = 'mnu'
+                if away == 'STC':
+                    s_away = 'stc'
+
+                if away == 'CHE':
+                    s_away = 'che'
+                if away == 'WAT':
+                    s_away = 'wat'
+                if away == 'BRI':
+                    s_away = 'bri'
+                if away == 'NWC':
+                    s_away = 'nwc'
+                if away == 'BOU':
+                    s_away = 'bou'
+
+                if away == 'SWA':
+                    s_away = 'swa'
+                if away == 'BUR':
+                    s_away = 'bur'
+                if away == 'ARS':
+                    s_away = 'ars'
+                if away == 'TOT':
+                    s_away = 'tot'
+                if away == 'EVE':
+                    s_away = 'eve'
+
+                if away == 'HUD':
+                    s_away = 'hud'
+                if away == 'LIV':
+                    s_away = 'liv'
+                if away == 'CRY':
+                    s_away = 'cry'
+                if away == 'SOU':
+                    s_away = 'sou'
+                if away == 'MNC':
+                    s_away = 'mnc'
+
+
+                f.write(home + "," + score + "," + away + "," + result(score) + "," + home_score(score) + "," + away_score(score) + "," + goal_goal(score) + "," + over_1(score) + "," + over_2(score) + "," + over_3(score) + "," + over_4(score) + "," + hodd(s_home,s_away) + "," + dodd(s_home,s_away) + "," + aodd(s_home,s_away) + "\n")
 
     print(">>database/" + filename)
 
--- extraction/utils.py
+++ extraction/utils.py
@@ -31,7 +31,7 @@ def goal_goal(score):
     if score == '1-2' or score == '1-3' or score == '2-3' or score == '1-4' or score == '2-4' or score == '1-5':
         return('1')
     if score == '1-1' or score == '2-2' or score == '3-3':
-        return('1')    
+        return('1')
 
 def over_1(score):
     if score == '0-0' or score == '0-1' or score == '1-0':
@@ -62,7 +62,34 @@ def over_4(score):
         return('0')
     else:
         return('1')
-	
+
+def hodd(h,a):
+    f = open('odd2.csv', 'r')
+    for line in f:
+        row = line.split(',')
+        if h in row[0] and a in row[0]:
+            h_ = str(row[1])
+            return(h_)
+    f.close()
+
+def dodd(h,a):
+    f = open('odd2.csv', 'r')
+    for line in f:
+        row = line.split(',')
+        if h in row[0] and a in row[0]:
+            d_ = str(row[2])
+            return(d_)
+    f.close()
+
+def aodd(h,a):
+    f = open('odd2.csv', 'r')
+    for line in f:
+        row = line.split(',')
+        if h in row[0] and a in row[0]:
+            a_ = str(row[3])
+            return(a_)
+    f.close()
+
 def res(score):
     ##home_wins
     if score == '1-0' or score == '2-1' or score == '2-0' or score == '3-1' or score == '3-0' or score == '3-2':
